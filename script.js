daefb33fe496af727a4040965a5dec7d6e76315e
pedrorosadomingues <pedrorosadomingues@gmail.com>
2022-10-26 23:37:31 -0300

    projeto pronto sem bonus sem ajustes finos

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
 const nomeUsuario = prompt('Qual seu nome?')
 
-const login = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: nomeUsuario})
-login.then(buscarMensagens)
+
+
+const login = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', { name: nomeUsuario })
+login.then(searchMessages)
 login.catch(alertError)
 
-function buscarMensagens() {
-    const mensages = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+function searchMessages() {
+    const messages = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
 
-mensages.then(renderMensages)
-mensages.catch(alertError)
-
+    messages.then(renderMessages)
+    messages.catch(alertError)
 }
+
+function checkStatus() {
+    const keepOnline = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', { name: nomeUsuario })
+    keepOnline.then(searchMessages)
+    keepOnline.catch(alertError)
+    console.log('keepOnline')
+}
+
+setInterval(checkStatus, 5000)
 
 let chat = document.querySelector('.chat')
 
-
-
-function renderMensages(response) {
-    const mensages = response.data
-    console.log(mensages)
-    for ( i = 0; i < mensages.length; i++) {
-        if (mensages[i].text === 'entra na sala...' || mensages[i].text === 'sai da sala...') {
-        chat.innerHTML += `<li> (${mensages[i].time})  <strong> ${mensages[i].from} </strong> ${mensages[i].text}
+function renderMessages(response) {
+    const messages = response.data
+    console.log(messages)
+    for (i = 0; i < messages.length; i++) {
+        if (messages[i].text === 'entra na sala...' || messages[i].text === 'sai da sala...') {
+            chat.innerHTML += `<li> (${messages[i].time})  <strong> ${messages[i].from} </strong> ${messages[i].text}
         </li>`
-    } else {
-        chat.innerHTML += `<li> (${mensages[i].time}) <strong>${mensages[i].from}</strong> para <strong>${mensages[i].to}:</strong> ${mensages[i].text}</li>`
+        } else {
+            chat.innerHTML += `<li> (${messages[i].time}) <strong>${messages[i].from}</strong> para <strong>${messages[i].to}:</strong> ${messages[i].text}</li>`
+        }
     }
-}
-let lastMensage = document.querySelector('ul :nth-last-child(3)')
+    let lastMessage = document.querySelector('ul :nth-last-child(3)')
 
-lastMensage.scrollIntoView()
+    lastMessage.scrollIntoView()
 }
 
 function alertError(erro) {
-    if(erro.response.status === 404 )  {
-    alert('Erro ao carregar mensagens')
+    if (erro.response.status === 404) {
+        alert('Erro ao carregar mensagens')
     }
-    if(erro.response.status === 400) {
-        alert('Nome de usuário já existe')
+    if (erro.response.status === 400) {
+        alert(erro.message)
+
     }
+    console.log(erro)
 }
 
+function sendMessage() {
+    let message = document.querySelector('input').value
+    let cleanMessage = document.querySelector('input')
 
+
+    console.log(message)
+    const sendingMessage = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', { from: nomeUsuario, to: 'Todos', text: message, type: 'message' })
+
+    sendingMessage.then(searchMessages)
+    sendingMessage.catch(alertError)
+    cleanMessage.value = ''
+}
 
